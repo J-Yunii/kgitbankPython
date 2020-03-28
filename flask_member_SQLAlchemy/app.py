22b2a57556896beaddb403a16ef93c46fda47457
JungYun Choi <49744526+J-Yunii@users.noreply.github.com>
2020-03-28 09:24:40 +0900

    ddd

--- flask_member_SQLAlchemy/app.py
+++ flask_member_SQLAlchemy/app.py
@@ -1,13 +1,7 @@
 from flask import Flask,request,render_template,redirect,url_for,jsonify
+import pymysql,os
 from flask_sqlalchemy import SQLAlchemy
-import os
 
-
-# pymysql => sqlalchmy로 바꿔서
-
-# 서버에 있는 소스를 클라이언트 웹에 가져와서 실행
-# html: 정적인 문서, 골격 잡는데 한계 => CSS로 보완
-# 자바스크립터: 동적인 문서
 app=Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:qwer1234@maria/test"
@@ -17,33 +11,29 @@ db = SQLAlchemy(app)
 
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    userid = db.Column(db.String(20))
+    #id =  db.Column(db.Integer, primary_key = True)
+    userid = db.Column(db.String(20),primary_key = True)
     userpw = db.Column(db.String(20))
     username = db.Column(db.String(20))
     userage = db.Column(db.Integer)
     usermail = db.Column(db.String(20))
-    useradd = db.Column(db.String(50))
+    useradd = db.Column(db.String(20))
     usergender = db.Column(db.String(20))
     usertel = db.Column(db.String(20))
 
-    # 생성자에 id 넣으면 안됨
     def __init__(self,userid,userpw,username,userage,usermail,useradd,usergender,usertel):
-        self.userid = userid
-        self.userpw = userpw
-        self.username = username
-        self.userage = userage
-        self.usermail = usermail
-        self.useradd = useradd
-        self.usergender = usergender
-        self.usertel = usertel
+        self.userid=userid
+        self.userpw=userpw
+        self.username=username
+        self.userage=userage
+        self.usermail=usermail
+        self.useradd=useradd
+        self.usergender=usergender
+        self.usertel=usertel
+
 
 @app.route('/')
 def index():
-    return render_template('bootstraptest.html')
-
-@app.route('/bootstrap')
-def bootstraptest():
     return render_template('bootstraptest.html')
 
 @app.route('/form')
@@ -57,8 +47,10 @@ def formresult():
     joblist=request.form.getlist('job')
     return render_template('formresult.html',username=username,userpass=userpass,joblist=joblist) 
 
+@app.route('/bootstrap')
+def bootstraptest():
+    return render_template('bootstraptest.html')
 
-# 인설트!
 @app.route('/usersform',methods=['POST','GET'])
 def usersform():
     if request.method == 'GET':
@@ -72,78 +64,65 @@ def usersform():
         useradd = request.form.get('useradd')
         usergender = request.form.get('usergender')
         usertel = request.form.get('usertel')
-        
-        my_data = User(userid,userpw,username,userage,usermail,useradd,usergender,usertel)
-        db.session.add(my_data)
+    
+        my_user=User(userid,userpw,username,userage,usermail,useradd,usergender,usertel)       
+        db.session.add(my_user)              
         db.session.commit()
 
     return redirect('/list')
 
-# sql="select * from users where userid = %s;"
 @app.route('/updateform/<userid>',methods=['GET'])
 def updateformget(userid):
-    selected_data = User.query.get(request.form.args('userid'))
+    result=User.query.filter_by(userid = userid).one()
+    return render_template('updateform.html',list=result)  
 
-    return render_template('updateform.html',list=selected_data)  
-
-
-# userid를 조건으로 update
 @app.route('/updateform',methods=['POST'])
 def updateformpost():
+    my_user = User.query.get(request.form.get('userid'))   
+    
+    my_user.userid = request.form.get('userid')
+    my_user.userpw = request.form.get('userpw')
+    my_user.username = request.form.get('username')
+    my_user.userage = request.form.get('userage')
+    my_user.usermail = request.form.get('usermail')
+    my_user.useradd = request.form.get('useradd')
+    my_user.usergender = request.form.get('usergender')
+    my_user.usertel = request.form.get('usertel')  
 
-    my_data = User.query.get(request.form.get('userid'))
+    db.session.commit()
 
-    my_data.userid = request.form.get('userid')
-    my_data.userpw = request.form.get('userpw')
-    my_data.username = request.form.get('username')
-    my_data.userage = request.form.get('userage')
-    my_data.usermail = request.form.get('usermail')
-    my_data.useradd = request.form.get('useradd')
-    my_data.usergender = request.form.get('usergender')
-    my_data.usertel = request.form.get('usertel')
-
-    db.session.commit()                      
     return redirect('/list')    
 
-
-
-# userid값으로 select하기
-# userid 주소로 받아오는 거 주의
-# data는 괄호 안에! 쀼쀼
 @app.route('/content/<userid>')
 def content(userid):
-    my_data = User.query.get(request.form.args('userid'))
-    
-    return render_template('content.html',list=my_data)
+    result=User.query.filter_by(userid = userid).one()
+    return render_template('content.html',list=result)
 
 
-# userid를 key값으로 지우기
 @app.route('/deleteform/<userid>')
 def deleteformget(userid):
     my_data = User.query.get(userid)
     db.session.delete(my_data)
     db.session.commit()
-
     return redirect('/list')  
 
 @app.route('/list')
 def list():
-    all_data = User.query.all()
+    all_data=User.query.all()
     return render_template('list.html',list=all_data)      
 
-# select all
 @app.route('/ajaxlist',methods=['GET'])
 def ajaxlistget():
-    all_data = User.query.all()
-
+    all_data=User.query.all()
     return render_template('ajaxlist.html',list=all_data)      
 
-@app.route('/ajaxlist', methods=['POST'])
+@app.route('/ajaxlist',methods=['POST'])
 def ajaxlistpost():
-    inputid = request.form.get('userid')
-    inputid = '%'+inputid+'%'
-    selected_data = User.query.filter_by(User.userid.like(inputid)).all()
-    return jsonify(selected_data)    
+    userid = request.form.get('userid')
+    query = User.query.filter(User.userid.like('%'+userid+'%')). order_by(User.userid)
+    all_data = query.all()
+   
+    return jsonify(all_data)    
 
 @app.route('/imglist')
 def imglist():
--- flask_member_SQLAlchemy/db_create.py
+++ flask_member_SQLAlchemy/db_create.py
@@ -1,5 +1,4 @@
+# 테이블 생성 맨처음 한번 실행
 from app import db
 
-# conda install -c conda-forge flask_sqlalchemy로설치하기
-# 테이블을 만듬
 db.create_all()
